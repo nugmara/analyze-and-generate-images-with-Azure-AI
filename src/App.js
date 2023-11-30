87c80ff675ff9c4fde1118ea9981e17fce751dd1
nugmara <dagmarag0@gmail.com>
2023-11-30 21:51:07 +0100

    Design the graphical user interface of our application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+const [input, setInput] = useState("");
+
+
+
+const handleInputChange = (e) => { 
+  setInput(e.target.value);
+}
+
+const handleAnalysisButtonClick = () => { 
+  // Add code here to analyze the image
+  console.log("Analyzing image: ", input);
+}
+
+const handleGenerationButtonClick = () => { 
+  // Add code here to generate an image
+  console.log("Generating image: ", input);
+}
+
+  return (
+    <div> 
+    
+      <h1>Computer Vision</h1>
+
+      <label htmlFor="imageUrl">Enter the image URL or Prompt:</label>
+      <input 
+      type="text" 
+      id="imageUrl"
+      value={input}
+      onChange={handleInputChange}
+      />
+
+      <br />
+
+      <button onClick={handleAnalysisButtonClick}>Analyse Image</button>
+      <button onClick={handleGenerationButtonClick}>Generate image</button>
+    
+    </div>
+
+  )
 }
 
 export default App;
